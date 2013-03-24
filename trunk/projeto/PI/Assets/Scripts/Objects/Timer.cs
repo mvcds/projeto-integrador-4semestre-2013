c453e04fbaa8cf9a8731923f8979443e85b32204
rafael.fpaz@gmail.com <rafael.fpaz@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-03-24 14:29:49 +0000

    # NPC's e Quests básicas.

---
+++ trunk/projeto/PI/Assets/Scripts/Objects/Timer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class QuestTimer {
+
+	private static long tempo;
+	private static int secs;
+   
+    public QuestTimer(int segundos){
+        tempo = (long)Time.time;
+		secs = segundos;
+    }
+	
+    public bool checkTime() {
+        if ((int)(secs - (Time.time - tempo))%60 <= 0){
+            if ((int)(secs - (Time.time - tempo))/60 <= 0){
+                // Acabou o tempo da quest
+                return true;
+            }
+        }
+		return false;
+    }
+	
+	
+    public int getSegundos(){
+        return (int)(secs - (Time.time - tempo))%60 ;
+    }
+    
+    public int getMinutos(){
+        return (int)(secs - (Time.time - tempo))/60 ;
+    }
+	
+	public string getTempo(){
+		string t;
+		
+		if (getSegundos() >= 0){
+			t = string.Format ("{0:00} : {1:00}", getMinutos(), getSegundos());
+		} else {
+			t = "00 : 00";
+		}
+		 
+		return t;
+	}
+}
--- trunk/projeto/PI/Assets/Standard Assets/LoadNextLevel.cs
+++ trunk/projeto/PI/Assets/Standard Assets/LoadNextLevel.cs
@@ -10,8 +10,12 @@ public class LoadNextLevel : MonoBehaviour {
 		if(stageCleared){
 			GUI.Box (new Rect (Screen.width / 2 - 200, Screen.height / 3 - 150, 400, 300), "Stage Clear");
 
-			if (GUI.Button (new Rect ( Screen.width /2 - 50, Screen.height / 2, 100, 20), "Next Level")) {
+			if (GUI.Button (new Rect ( Screen.width /2 - 150, Screen.height / 2, 100, 20), "Next Level")) {
 				Application.LoadLevel(Application.loadedLevel  + 1);
+				stageCleared = false;
+			}
+			
+			if (GUI.Button (new Rect ( Screen.width /2 + 50, Screen.height / 2, 100, 20), "Stay")) {
 				stageCleared = false;
 			}
 

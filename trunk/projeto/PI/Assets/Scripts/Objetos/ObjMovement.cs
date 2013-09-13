9dd726082be80927623ff4cc297feed0cc2b5417
rafael.fpaz@gmail.com <rafael.fpaz@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-09-13 00:47:39 +0000

    Fios, Boia e Blocos.

---
+++ trunk/projeto/PI/Assets/Scripts/Objetos/ObjMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObjMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Boia){
+			transform.Translate((-Vector3.forward * Time.deltaTime * MainScript.gameVelocity) / MainScript.floatSpeed);
+		} else {
+			transform.Translate(-Vector3.forward * Time.deltaTime * MainScript.gameVelocity);
+		}
+		
+		if (transform.position.z < -10){
+			Destroy(gameObject);
+		}
+	}
+	
+	 void OnTriggerEnter(Collider other) {
+        Destroy(transform.gameObject);
+		PlayerStatus.gotHit();
+    }
+}

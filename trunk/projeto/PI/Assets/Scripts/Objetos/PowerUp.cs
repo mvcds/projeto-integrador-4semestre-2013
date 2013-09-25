8e629037b6ee27e9c54512ecfbea3454b4f16dae
lucasachao@gmail.com <lucasachao@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-09-25 00:55:39 +0000

    sound scripts created. ducksound is not completed yet

--- trunk/projeto/PI/Assets/Scripts/Objetos/PowerUp.cs
+++ trunk/projeto/PI/Assets/Scripts/Objetos/PowerUp.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class PowerUp : MonoBehaviour {
 	
 	public int powerUpID;
-	public AudioClip sound;//sound played after collision with powerup
-	
+		
 	// Use this for initialization
 	void Start () {
 	
@@ -13,19 +12,10 @@ public class PowerUp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Boia){
-			transform.Translate((-Vector3.forward * Time.deltaTime * MainScript.gameVelocity) / MainScript.floatSpeed);
-		} else {
-			transform.Translate(-Vector3.forward * Time.deltaTime * MainScript.gameVelocity);
-		}
-		
-		if (transform.position.z < -10){
-			Destroy(gameObject);
-		}
+
 	}
 	
 	 void OnTriggerEnter(Collider other) {
-		AudioSource.PlayClipAtPoint(sound, transform.position);
 		Destroy(transform.gameObject);
 		PlayerStatus.gotPowerUp(powerUpID);
     }

c22ae5546b052146ada8b1441b668306f37e6610
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-10-09 00:43:46 +0000

    Sepparating Scripts by function (as components demands)

---
+++ projeto/PI/Assets/Scripts/Objetos/Obstacle.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[RequireComponent(typeof(ObjMovement))]
+public class Obstacle : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+        Destroy(transform.gameObject);
+		PlayerStatus.gotHit();
+    }
+}
--- projeto/PI/Assets/Scripts/Objetos/PowerUp.cs
+++ projeto/PI/Assets/Scripts/Objetos/PowerUp.cs
@@ -1,30 +1,12 @@
 using UnityEngine;
-using System.Collections;
-using System;
 
+[RequireComponent(typeof(ObjMovement))]
 public class PowerUp : MonoBehaviour {
 	
-	public int powerUpID;
+	public int powerUpID;		
 		
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Boia){
-			transform.Translate((-Vector3.forward * Time.deltaTime * MainScript.gameVelocity) / MainScript.floatSpeed);
-		} else {
-			transform.Translate(-Vector3.forward * Time.deltaTime * MainScript.gameVelocity);
-		}
-		
-		if (transform.position.z < -10){
-			Destroy(gameObject);
-		}
-	}
-	
-	 void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter(Collider other)
+	{
 		Destroy(transform.gameObject);
 		PlayerStatus.gotPowerUp(powerUpID);
     }

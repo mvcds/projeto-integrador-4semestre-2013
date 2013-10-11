de9767842fd83f83e707a44be0b2cc78aa32e118
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-10-11 00:47:19 +0000

    Before scenes and splash

---
+++ branches/screen-change-with-block-before-scene/PI/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	
+	public Button[] buttons;
+	
+	void Update()
+	{
+		Show();
+	}
+
+	protected void Show()
+	{
+		foreach(Button btn in buttons)
+		{
+			if (btn == null)
+				continue;
+			//btn.Show(Diretor.Instancia.TelaAtual == MenuTela());
+			btn.Show(true);
+		}
+	}
+}

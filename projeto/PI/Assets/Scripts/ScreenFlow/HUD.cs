506a1b375c218e5ff234334c7c0c17130227ddc2
rafael.fpaz@gmail.com <rafael.fpaz@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-10-21 23:58:23 +0000

    Barra do PowerUp.

--- projeto/PI/Assets/Scripts/ScreenFlow/HUD.cs
+++ projeto/PI/Assets/Scripts/ScreenFlow/HUD.cs
@@ -9,7 +9,11 @@ public class HUD : MonoBehaviour {
 	private Texture heartEmpty;
 	private Texture heartFull;
 	private Texture duckBar;
-		
+	
+	private Texture powerUpBarraPorta;
+	private Texture powerUpBarraBoia;
+	private Texture powerUpBarraCapivara;
+	private Texture powerUpFill;
 	
 	// Use this for initialization
 	void Start ()
@@ -18,17 +22,22 @@ public class HUD : MonoBehaviour {
 		heartEmpty = (Texture)Resources.Load("Images/HUD/LifeBar/vida_vazio");
 		heartFull = (Texture)Resources.Load("Images/HUD/LifeBar/vida_cheio");
 		
-		duckBar = (Texture)Resources.Load("Images/HUD/tela_jogo_pato");		
+		duckBar = (Texture)Resources.Load("Images/HUD/tela_jogo_pato");	
+		
+		powerUpBarraBoia = (Texture)Resources.Load("Images/HUD/LifeBar/BarraBoia");
+		powerUpBarraCapivara = (Texture)Resources.Load("Images/HUD/LifeBar/BarraCapivara");
+		powerUpBarraPorta = (Texture)Resources.Load("Images/HUD/LifeBar/BarraPorta");
+		powerUpFill = (Texture)Resources.Load("Images/HUD/LifeBar/greenBar");
 	}
 	
 	void OnGUI(){
 		if (!Director.Instance.isRunning)
 			return;
 			
-		if (PlayerStatus.duration > 0)
-			GUI.Box(new Rect(10, 10, 120, 25), "PowerUp: " + (int)(PlayerStatus.duration + 1) + " / " + PlayerStatus.maxDuration);
+		//if (PlayerStatus.duration > 0)
+			//GUI.Box(new Rect(10, 10, 120, 25), "PowerUp: " + (int)(PlayerStatus.duration + 1) + " / " + PlayerStatus.maxDuration);
 		GUI.Box(new Rect(Screen.width / 2 - 60, 10, 120, 25), "Velocidade: " + (int)MainScript.gameVelocity);
-		GUI.Box(new Rect(10, Screen.height - 35, 200, 25), "Distancia Percorrida: " + (int) Director.Instance.GameRank.Distance);
+		GUI.Box(new Rect(Screen.width - 250, Screen.height - 60, 200, 25), "Distancia Percorrida: " + (int) Director.Instance.GameRank.Distance);
 		
 		// Ducks
 		GUIStyle myStyle = new GUIStyle();
@@ -38,9 +47,27 @@ public class HUD : MonoBehaviour {
 		myStyle.normal.textColor = Color.white;	
 					
 		drawImage(0, Screen.height * 0.05f, duckBar);
-        GUI.Label(new Rect(-280, Screen.height * 0.07f, 500, 50), "" + Director.Instance.GameRank.Ducks, myStyle);
-		//GUI.Label(new Rect(-280, Screen.height * 0.06f, 500, 50), "" + MainScript.ducks, myStyle);
-				
+        //GUI.Label(new Rect(-280, Screen.height * 0.07f, 500, 50), "" + Director.Instance.GameRank.Ducks, myStyle);
+		GUI.Label(new Rect(-280, Screen.height * 0.065f, 500, 50), "" + Director.Instance.GameRank.Ducks, myStyle);
+		
+		// PowerUp
+		if (PlayerStatus.powerUp != PlayerStatus.PowerUp.Nada){
+			
+			GUI.DrawTexture (new Rect (50 + 40, Screen.height * 0.912f, (PlayerStatus.duration / PlayerStatus.maxDuration) * powerUpFill.width, powerUpFill.height), powerUpFill);
+			
+			//drawImage(50, Screen.height * 0.90f, powerUpFill);
+			if (PlayerStatus.powerUp != PlayerStatus.PowerUp.Capivara)
+				drawImage(50, Screen.height * 0.90f, powerUpBarraCapivara);
+			
+			if (PlayerStatus.powerUp != PlayerStatus.PowerUp.Porta)
+				drawImage(50, Screen.height * 0.90f, powerUpBarraPorta);
+			
+			if (PlayerStatus.powerUp != PlayerStatus.PowerUp.Boia)	
+				drawImage(50, Screen.height * 0.90f, powerUpBarraBoia);
+			
+			
+		}
+		
 		// Life Bar
 		drawImage(Screen.width - (lifeBar.width), Screen.height * 0.05f, lifeBar);
 		drawHeart(1);
@@ -51,9 +78,9 @@ public class HUD : MonoBehaviour {
 	
 	void drawHeart(int position){
 		if (PlayerStatus.vida >= position)
-			drawImage(Screen.width - (lifeBar.width / 2) - 180 + (position * 80), Screen.height * 0.05f, heartFull);
+			drawImage(Screen.width - (lifeBar.width / 2) - 180 + (position * 80), Screen.height * 0.065f, heartFull);
 		else
-			drawImage(Screen.width - (lifeBar.width / 2) - 180 + (position * 80), Screen.height * 0.05f, heartEmpty);	
+			drawImage(Screen.width - (lifeBar.width / 2) - 180 + (position * 80), Screen.height * 0.065f, heartEmpty);	
 	}
 	
 	void drawImage(float x, float y, Texture texture){

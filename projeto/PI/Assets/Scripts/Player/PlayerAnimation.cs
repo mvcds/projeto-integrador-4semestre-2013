c81b3ed37eaf30745f0f58276304a0bed83686f8
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-11-16 18:34:54 +0000

    Capivara Otimização da animação

--- projeto/PI/Assets/Scripts/Player/PlayerAnimation.cs
+++ projeto/PI/Assets/Scripts/Player/PlayerAnimation.cs
@@ -5,27 +5,15 @@ public class PlayerAnimation : MonoBehaviour {
 	
 	public GameObject nadando;
 	public GameObject boiando;
-	public GameObject naPorta;
+    public GameObject naPorta;
+    public GameObject capivara;
 	
 	void Update(){
-		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Boia){
-			nadando.SetActiveRecursively(false);
-			boiando.SetActiveRecursively(true);
-			naPorta.SetActiveRecursively(false);
-		}
-		
-		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Porta){
-			nadando.SetActiveRecursively(false);
-			boiando.SetActiveRecursively(false);
-			naPorta.SetActiveRecursively(true);
-		}
-		
-		if (PlayerStatus.powerUp == PlayerStatus.PowerUp.Nada){
-			print("entrou");
-			
-			nadando.SetActiveRecursively(true);
-			boiando.SetActiveRecursively(false);
-			naPorta.SetActiveRecursively(false);
-		}
+
+        nadando.SetActiveRecursively(PlayerStatus.powerUp == PlayerStatus.PowerUp.Nada);
+        boiando.SetActiveRecursively(PlayerStatus.powerUp == PlayerStatus.PowerUp.Boia);
+        naPorta.SetActiveRecursively(PlayerStatus.powerUp == PlayerStatus.PowerUp.Porta);
+        capivara.SetActiveRecursively(PlayerStatus.powerUp == PlayerStatus.PowerUp.Capivara);
+
 	}
 }

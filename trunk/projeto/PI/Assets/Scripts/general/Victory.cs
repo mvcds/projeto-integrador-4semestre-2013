0a0af018ef034b244d62f65950be78d3df9fe182
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-11-17 19:40:47 +0000

    Condição de vitória/derrota

---
+++ trunk/projeto/PI/Assets/Scripts/general/Victory.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Linq;
+
+public class Victory : MonoBehaviour {
+
+    public Font font;
+
+    public Rect LevelPosition, ObjectivePosition;
+    public bool foreverMessage;
+
+    public static string LevelObjective;
+
+    public WinningCondition[] conditions;
+    public float messageTime = 5;
+    private float missionMessageTime;
+    public int fase;
+
+    void Start()
+    {
+        Validate();
+        missionMessageTime = messageTime * conditions.Length;
+    }
+
+    void Update()
+    {
+        Validate();
+        FixPosition();
+        
+        if (conditions.Where(p => !p.hasWon).Count() == 0)
+            Director.Instance.GameOver(true);
+    }
+
+    void Validate()
+    {
+        if (conditions.Length <= 0)
+            throw new Exception("No winnign condition");
+    }
+
+    void FixPosition()
+    {
+        LevelPosition.x = ScreenUtil.FixForHundred(LevelPosition.x, true);
+        LevelPosition.y = ScreenUtil.FixForHundred(LevelPosition.y, true);
+        LevelPosition.width = ScreenUtil.FixForHundred(LevelPosition.width);
+        LevelPosition.height = ScreenUtil.FixForHundred(LevelPosition.height);
+
+        ObjectivePosition.x = ScreenUtil.FixForHundred(ObjectivePosition.x, true);
+        ObjectivePosition.y = ScreenUtil.FixForHundred(ObjectivePosition.y, true);
+        ObjectivePosition.width = ScreenUtil.FixForHundred(ObjectivePosition.width);
+        ObjectivePosition.height = ScreenUtil.FixForHundred(ObjectivePosition.height);
+    }
+
+    void OnGUI()
+    {
+        if (!Director.Instance.isRunning)
+            return;
+
+        if (!(Debug.isDebugBuild && foreverMessage))
+            missionMessageTime -= Time.deltaTime;
+
+        //* Label Inicial
+        if (missionMessageTime > 0)
+        {
+            GUIStyle myStyle = new GUIStyle();
+            myStyle.font = font;
+            myStyle.fontSize = 27;
+            myStyle.alignment = TextAnchor.MiddleCenter;
+            myStyle.normal.textColor = Color.white;
+            myStyle.border.bottom = 1;
+
+            Rect lp = new Rect(ScreenUtil.FitOnWidth(LevelPosition.x), ScreenUtil.FitOnHeight(LevelPosition.y),
+                ScreenUtil.FitOnWidth(LevelPosition.width), ScreenUtil.FitOnHeight(LevelPosition.height));
+            GUI.Label(lp, "FASE " + fase, myStyle);
+
+            myStyle.fontSize = 23;
+            Rect op = new Rect(ScreenUtil.FitOnWidth(ObjectivePosition.x), ScreenUtil.FitOnHeight(ObjectivePosition.y),
+                ScreenUtil.FitOnWidth(ObjectivePosition.width), ScreenUtil.FitOnHeight(ObjectivePosition.height));
+            GUI.Label(op, Objective, myStyle); 
+		}
+		//*/
+    }
+
+    private string Objective
+    {
+        get
+        {
+            string obj = "";
+
+            foreach (WinningCondition win in conditions)
+                TreatString(ref obj, win.msg);
+
+            LevelObjective = obj;
+
+            return LevelObjective;
+        }
+    }
+
+
+    void TreatString(ref string str, string appended)
+    {
+        if (!string.IsNullOrEmpty(str))
+            str += "\n";
+
+        str += appended;
+    }
+}
---
+++ trunk/projeto/PI/Assets/Scripts/general/WinningCondition.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class WinningCondition : MonoBehaviour {
+
+    public enum Unit
+    {
+        Duck
+        , Distance/*
+        , Time
+        , Hit
+        , GotPowerUp
+        , GotCapivara
+        , GotBoia
+        , GotPorta
+        , KeepPowerUp
+        , KeepCapivara
+        , KeepBoia
+        , KeepPorta
+        , LosePowerUp
+        , LoseCapivara
+        , LoseBoia
+        , LosePorta*/
+    }
+
+    public enum Condition
+    {
+        Less,
+        LessEqual,
+        Equal,
+        More,
+        MoreEqual,
+        Diferent
+    }
+    
+    public bool hasWon
+    {
+        get;
+        private set;
+    }
+
+    public Unit unit;
+    public Condition condition;
+    public float value;
+    public string msg = "Just do it!";
+
+    /// <summary>
+    /// Se depois de ter alcançado a condição ela for perdida, o jogo acaba
+    /// </summary>
+    public bool failable;
+    
+	// Use this for initialization
+	void Start () {
+        hasWon = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasWon)
+            hasWon = Test;
+        else if (failable)
+        {
+            if (!Test)
+                Director.Instance.GameOver(false);
+        }
+	}
+
+    bool Test
+    {
+        get
+        {
+            switch (condition)
+            {
+                case Condition.Diferent:
+                    return (getUnitValue != value);
+                case Condition.Equal:
+                    return (getUnitValue == value);
+                case Condition.Less:
+                    return (getUnitValue < value);
+                case Condition.LessEqual:
+                    return (getUnitValue <= value);
+                case Condition.More:
+                    return (getUnitValue > value);
+                case Condition.MoreEqual:
+                    return (getUnitValue >= value);
+            }
+            throw new Exception("Test against what?");
+        }
+    }
+
+    float getUnitValue
+    {
+        get
+        {
+            switch (unit)
+            {
+                case Unit.Distance:
+                    return Director.Instance.GameRank.Distance;
+                case Unit.Duck:
+                    return Director.Instance.GameRank.Ducks;
+                /*case Unit.Time:
+                    return 0;*/
+            }
+            throw new Exception("Get Value of?");
+        }
+    }
+}

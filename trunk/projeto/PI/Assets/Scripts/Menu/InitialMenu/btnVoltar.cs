6cf525ba9a2c960a67f44faed8054dabe529b132
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-11-20 17:37:35 +0000

    #Tela de tutorial

---
+++ trunk/projeto/PI/Assets/Scripts/Menu/InitialMenu/btnVoltar.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class btnVoltar : Button {
+	
+	protected override void Action ()
+    {
+        base.Action();
+
+        _myMenu._bgMenu = null;
+        _myMenu.SetAble();
+	}
+	
+}

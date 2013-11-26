e42dbf099adcffd2619c6fc8cef93ed0f5efebc2
blackfenrir.nobody <blackfenrir.nobody@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-11-26 00:13:05 +0000

    Alguns blocos modificados e nova build do jogo

--- projeto/PI/Assets/Scripts/general/MainScript.cs
+++ projeto/PI/Assets/Scripts/general/MainScript.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class MainScript {
 
-	public static float gameVelocity = 10;
+	public static float gameVelocity = minspeed;
 	public static float floatSpeed = 1.5f;
 	public static float Maxfolego = 3.0f;
 	public static float folego = Maxfolego;
 	public static string levelToLoad;
 	public static int maxspeed = 15 ;
-	public static int minspeed = 10;
+	public static int minspeed = 8;
 	
 	public static void loadLevel(string level){
 		levelToLoad = level;

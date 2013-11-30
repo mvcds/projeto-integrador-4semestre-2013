544636820d43dc4054644e424964e2a06ccb6dfa
marvinc.silva@gmail.com <marvinc.silva@gmail.com@5b680968-edd5-d652-6c80-29b13895f6ec>
2013-11-30 12:27:20 +0000

    Ranking preparado pra entrada de ranking

--- trunk/projeto/PI/Assets/Scripts/ScreenFlow/HUDRanking.cs
+++ trunk/projeto/PI/Assets/Scripts/ScreenFlow/HUDRanking.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HUDRanking : MonoBehaviour {
 	
@@ -9,15 +10,10 @@ public class HUDRanking : MonoBehaviour {
 	private int paddingY = (int) (Screen.height * 0.1f);
 	private int boxWidth = (int) (Screen.width * 0.6f);
 	private int boxHeight = (int) (Screen.height * 0.08f);
-	
-	// Alterar Aqui
+
+    private List<Rank> ranks = new List<Rank>();
 	
 	private string title = "RANKING";
-	private string r1 = "Name1   Ducks: " + Director.Instance.GameRank.Ducks + " Distance: " + Director.Instance.GameRank.Distance;
-	private string r2 = "Name1   Ducks: " + Director.Instance.GameRank.Ducks + " Distance: " + Director.Instance.GameRank.Distance;
-	private string r3 = "Name1   Ducks: " + Director.Instance.GameRank.Ducks + " Distance: " + Director.Instance.GameRank.Distance;
-	private string r4 = "Name1   Ducks: " + Director.Instance.GameRank.Ducks + " Distance: " + Director.Instance.GameRank.Distance;
-	private string r5 = "Name1   Ducks: " + Director.Instance.GameRank.Ducks + " Distance: " + Director.Instance.GameRank.Distance;
 	
 	GUIStyle MyFont
     {
@@ -32,20 +28,26 @@ public class HUDRanking : MonoBehaviour {
         }
     }
 	
-	// Use this for initialization
-	void Start () {
-		
+	//Temporário
+	void Start () 
+    {
+        ranks.Add(new Rank(0, 0));
+        ranks.Add(new Rank(0, 0));
+        ranks.Add(new Rank(0, 0));
+        ranks.Add(new Rank(0, 0));
+        ranks.Add(new Rank(0, 0));
 	}
 	
-	void OnGUI(){
-		
+	void OnGUI()
+    {
 		GUI.Box(new Rect(x + ((boxWidth - boxWidth * 0.75f) / 2), y + (paddingY * -2), boxWidth * 0.75f, boxHeight), title, MyFont);
-		
-		GUI.Box(new Rect(x, y + (paddingY * 0), boxWidth, boxHeight), r1, MyFont);
-		GUI.Box(new Rect(x, y + (paddingY * 1), boxWidth, boxHeight), r2, MyFont);
-		GUI.Box(new Rect(x, y + (paddingY * 2), boxWidth, boxHeight), r3, MyFont);
-		GUI.Box(new Rect(x, y + (paddingY * 3), boxWidth, boxHeight), r4, MyFont);
-		GUI.Box(new Rect(x, y + (paddingY * 4), boxWidth, boxHeight), r5, MyFont);
-		
+
+        //TODO: nome?
+        for(int i = 0; i < ranks.Count; i++)
+        {
+            Rank rank = ranks[i];
+            string text = "Name1   Ducks: " + rank.Ducks + " Distance: " + rank.Distance;
+            GUI.Box(new Rect(x, y + (paddingY * i), boxWidth, boxHeight), text, MyFont);
+        }
 	}
 }
